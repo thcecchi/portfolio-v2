88b116cc169fa2a9edab8181b4aa76acde0354d9
Tyler Cecchi <thcecchi@gmail.com>
2016-08-22 19:17:25 -0400

    added a less dramatic hover shadow to project cards and changed border radius.

--- containers/Project.js
+++ containers/Project.js
@@ -27,12 +27,12 @@ class Project extends Component {
          transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
          backgroundColor: "#fff",
          width: "80%",
-         borderRadius: "5px",
+         borderRadius: "2px",
          marginLeft: "5%",
          padding: "1%",
          marginBottom: "3%",
          ":hover": {
-           boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+           boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
            cursor: "pointer"
          }
        }
